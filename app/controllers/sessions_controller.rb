b1b4c412358f9cadba9bd850a0fd63c7e82e3c6f
Bei Dai <bei.dai@kellogg.northwestern.edu>
2023-05-31 02:50:01 +0000

    draft

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({"email" => params["email"]})
+    if @user
+      password_the_user_typed = params["password"]
+      password_in_the_database = @user["password"]
+      if BCrypt::Password.new(password_in_the_database) == password_the_user_typed
+        redirect_to "/places"
+      else
+        flash["notice"] = "Incorrect password."
+        redirect_to "/sessions/new"
+      end
+    else
+      flash["notice"] = "Username not found."
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    flash["notice"] = "Goodbye."
+    session["user_id"] = nil
+    redirect_to "/login"
   end
 end
   
