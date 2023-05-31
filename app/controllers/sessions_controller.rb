48933f1462d5da6d719e2ed30d255b89bedaa1fa
Bei Dai <bei.dai@kellogg.northwestern.edu>
2023-05-31 03:00:40 +0000

    draft

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       password_the_user_typed = params["password"]
       password_in_the_database = @user["password"]
       if BCrypt::Password.new(password_in_the_database) == password_the_user_typed
+        session["user_id"] = @user["id"]
         redirect_to "/places"
       else
         flash["notice"] = "Incorrect password."
